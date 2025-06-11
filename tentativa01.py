c03b70eec11302726c537a16d6ec8bf18cb0346b
Rafael-Alexandrini <ralexandrini2@gmail.com>
2025-06-10 21:18:40 -0300

    Add Estrutura 4 e leitura ratings.csv

--- tentativa01.py
+++ tentativa01.py
@@ -1,6 +1,8 @@
 import pandas
+import itertools
+import time
 
-### Definição da Tabela Hash
+### Definição da Tabela Hash, Estrutura 1
 class HashTable: 
     def __init__(self, size):
         # Construtor
@@ -31,8 +33,46 @@ class HashTable:
         return False
 
 
+### Definição da Tabela Hash, Estrutura 3, 4 e P3
+class ListHashTable: 
+    # É uma tabela hash na qual múltiplos elementos podem
+    # ser inseridos com uma mesma chave
+    # estes são colocados em uma lista
+    def __init__(self, size):
+        # Construtor
+        self.size = size
+        self.table = [[] for i in range(size)]
+    
+    def _hashFunc(self, key):
+        # Função de Hash para int
+        return key % self.size
+
+    def insert(self, key, value):
+        # Insere entrada na tabela 
+        # Se key não for achada, insere na tabela como (chave, [lista de valores])
+        # Se key achada, insere value nessa lista de valores
+        index = self._hashFunc(key) 
+        for item in self.table[index]:
+            if key == item[0]:
+                item[1].append(value)
+                return
+            
+        self.table[index].append((key, [value]))
+    
+    def search(self, key):
+        # Procura pela entrada de chave key
+        # Retorna a lista de valores encontrada
+        # Caso não encontrada, retorna False
+        index = self._hashFunc(key) 
+        for entry in self.table[index]:
+            if key == entry[0]:
+                return entry[1]
+        # chave não encontrada
+        return False
+
 
 ### Leitura de movies.csv
+start_time = time.perf_counter()
 movies_file = pandas.read_csv("./movies.csv", 
                               dtype={"movieId":int, "title":str, "genres": str, "year": int})
 # Construção da tabela hash
@@ -42,11 +82,27 @@ for row in movies_file.itertuples(index=False):
     movie_list.extend([0, 0])
     movies_hash.insert(movie_list)
 
+### Leitura de miniratings.csv
+ratings_file = pandas.read_csv("./ratings.csv", 
+                               usecols=["userId", "movieId", "rating"], 
+                               dtype={"userId":int, "movieId":int, "rating":float})
+# Construção da estrutura 3
+ratings_hash = ListHashTable(80000)
+for row in ratings_file.itertuples(index=False):
+    ratings_hash.insert(row[0], (row[1], row[2]))
 
+end_time = time.perf_counter()
+build_time = (end_time - start_time)*1000
+print(build_time)
 
 # Testes
 if __name__ == "__main__":
     print(movies_hash.search(1))
     print(movies_hash.search(221))
     print(movies_hash.search(131262))
+    
+    print(pandas.DataFrame(ratings_hash.search(1), columns=["MovieId", "Rating"]))
+    #print(ratings_hash.search(99))
+    #print(ratings_hash.search(91))
+
     
