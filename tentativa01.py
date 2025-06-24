26472cd262dc602a2dd13405d1a1e9d78f37f1a8
Rafael-Alexandrini <ralexandrini2@gmail.com>
2025-06-24 18:53:07 -0300

    Pesquisa 1, Estrutura 1 e inserção mais rápida

--- tentativa01.py
+++ tentativa01.py
@@ -1,6 +1,7 @@
 import pandas
 import itertools
 import time
+import tentativaOrdenacao
 
 ### Definição da Tabela Hash, Estrutura 1
 class HashTable: 
@@ -96,7 +97,20 @@ class ListHashTable:
                 return
             
         self.table[index].append((key, [value]))
-    
+
+    def bulk_insert(self, key, value_list):
+        # inserção para vários items de uma mesma chave por vez
+        if self.keytype == "str":
+            index = self._strHash(key)     
+        else:
+            index = self._intHash(key) 
+        for item in self.table[index]:
+            if key == item[0]:
+                item[1].extend(value_list)
+                return
+            
+        self.table[index].append((key, value_list))
+
     def search(self, key):
         """Procura pela entrada de chave key
         Retorna a lista de valores encontrada
@@ -139,8 +153,83 @@ class ListHashTable:
             return 0
         return sum_lengths / n_filled    
 
+class TrieNode:
+    def __init__(self):
+        self.sons = [None for i in range(256)]
+        self.movieId = -1
+
+    def insert(self, title, movieId):
+        title_fixed = title.encode('latin1', errors='replace').decode('latin1') # para tirar char não ascii
+        self._insert(title_fixed, movieId)
+
+    def _insert(self, title, movieId):      
+        if len(title) == 0:
+            self.movieId = movieId
+        else:
+            if self.sons[ord(title[0])] == None:
+                self.sons[ord(title[0])] = TrieNode()
+            self.sons[ord(title[0])]._insert(title[1:], movieId)
+    
+    def search(self, title):
+        title_fixed = title.encode('latin1', errors='replace').decode('latin1') 
+        cur_node = self
+
+        for c in title_fixed:
+            if cur_node.sons[ord(c)] != None:
+                cur_node = cur_node.sons[ord(c)]
+            else:
+                return -1
+        
+        return cur_node.movieId
+    
+    def prefixSearch(self, prefix):
+        prefix_fixed = prefix.encode('latin1', errors='replace').decode('latin1') 
+        cur_node = self
+        # caminha pelo prefixo
+        for c in prefix_fixed:
+            if cur_node.sons[ord(c)] != None:
+                cur_node = cur_node.sons[ord(c)]
+            else:
+                return list()
+        # lista filmes da sub-árvore
+        return cur_node.listMovies()
 
 
+    def listMovies(self):
+        # Recursivamente lista filmes a partir de um nodo (inclusive ele)
+        movie_list = list()
+        if self.movieId != -1:
+            movie_list.append(self.movieId)
+        
+        for son in self.sons:
+            if son != None:
+                movie_list.extend(son.listMovies())
+
+        return movie_list
+
+            
+def pesquisa1(prefixo):
+    global movies_hash, title_trie
+
+    idList = title_trie.prefixSearch(prefixo)
+    
+    if len(idList) == 0:
+        print("Sem filmes com este prefixo!")
+        return
+    
+    moviesList = list()
+    for id in idList:
+        temp_movie = movies_hash.search(id)
+        if temp_movie != False:
+            moviesList.append(temp_movie)
+
+    if len(moviesList) == 0:
+        print("Não há filmes com este prefixo!") 
+        return
+    
+    for movie in tentativaOrdenacao.ordenar_por_nota_global(moviesList):
+        print(movie)
+    
 def pesquisa2(userID):
     global movies_hash, ratings_hash
     
@@ -160,10 +249,9 @@ def pesquisa2(userID):
         print("Sem filmes avaliados por esse usuário!")
         return
     
-    for movie in itertools.islice(moviesList, 20):
+    for movie in itertools.islice(tentativaOrdenacao.ordenar_por_nota_usuario_e_global(moviesList), 20):
         print(movie)
-    # moviesList é a lista de filmes avaliado pelo usuário, conforme a etapa 2 da documentação
-    # o que resta fazer é ordenar pela rating e global_rating e fazer print usando o pandas
+    # o que resta fazer é fazer print bonitinho
     
 def pesquisa3(n_filmes, genre):
     global movies_hash, genres_hash
@@ -183,11 +271,9 @@ def pesquisa3(n_filmes, genre):
         print("Não há filmes deste gênero!") 
         return
     
-    for movie in moviesList:
-        if movie[4] > 4:
-            print(movie)
-    # moviesList é a lista de filmes que tem o gênero, conforme a etapa 2 da documentação
-    # o que resta fazer é ordenar pela rating e fazer print dos n_filmes usando o pandas
+    for movie in itertools.islice(tentativaOrdenacao.ordenar_por_nota_global(moviesList), n_filmes):
+        print(movie)
+    # o que resta fazer é fazer print bonitinho
 
 def pesquisa4(tag1, tag2):
     global tags_hash, movies_hash
@@ -211,10 +297,9 @@ def pesquisa4(tag1, tag2):
         print("Sem filmes com ambas as tags!")
         return
     
-    for movie in moviesList:
+    for movie in tentativaOrdenacao.ordenar_por_nota_global(moviesList):
         print(movie)
-    # moviesList é a lista de filmes que tem ambas tags, conforme a etapa 3 da documentação
-    # o que resta fazer é ordenar pela rating e fazer print usando o pandas
+    # o que resta fazer é fazer print bonitinho
 
 
 # ===========================
@@ -230,6 +315,7 @@ movies_file = pandas.read_csv("./movies.csv",
 # Construção da tabela hash
 movies_hash = HashTable(10000)
 genres_hash = ListHashTable(20, keytype='str')
+title_trie = TrieNode()
 for row in movies_file.itertuples(index=False):
     movie_list = list(i for i in row)
     movie_list.extend([0, 0])
@@ -238,20 +324,35 @@ for row in movies_file.itertuples(index=False):
     for genre in movie_list[2].split("|"):
         genres_hash.insert(genre, movie_list[0])
 
+    title_trie.insert(row[1], row[0])
+
 
 
 ### Leitura de ratings.csv
-ratings_file = pandas.read_csv("./miniratings.csv", 
+ratings_file = pandas.read_csv("./ratings.csv", 
                             usecols=["userId", "movieId", "rating"], 
                             dtype={"userId":int, "movieId":int, "rating":float})
 # Construção da estrutura 3
 ratings_hash = ListHashTable(45000)
+temp_userID = -1
+temp_pair_list = list()
 for row in ratings_file.itertuples(index=False):
-    ratings_hash.insert(row[0], (row[1], row[2]))
+    #ratings_hash.insert(row[0], (row[1], row[2]))
+    # sistema para inserir vários ratings por vez
+    # fica uns 5 segundos mais rápido no meu computador :)
+    if row[0] == temp_userID:
+        temp_pair_list.append((row[1], row[2]))
+    else:
+        if len(temp_pair_list) > 0:
+            ratings_hash.bulk_insert(temp_userID, temp_pair_list)
+        temp_pair_list = [(row[1], row[2])]
+        temp_userID = row[0]
+
     temp_movie = movies_hash.search(row[1])
     if temp_movie != False:
         temp_movie[4] += row[2]
         temp_movie[5] += 1
+
 
 for bucket in movies_hash.table:
     for entry in bucket:
@@ -276,6 +377,34 @@ print("Fim")
 # ========================
 #   Cemitério dos Testes
 # ========================
+
+pesquisa1("America")
+    
+
+
+
+"""print(title_trie.search("Toy Story"))
+print(title_trie.search("Toy Story 2"))
+print(title_trie.search("Toy2"))
+print(title_trie.search("y"))
+print(title_trie.search("American Winter"))
+print(title_trie.search("H-Man, The (Bijo to Ekitainingen)"))"""
+
+
+#print(ratings_hash.search(1))
+
+"""print("Pesquisa 2:")
+pesquisa2(54766)
+print("Pesquisa 3:")
+pesquisa3(20, 'Mystery')
+print("Pesquisa 4:")
+pesquisa4("feel-good", "predictable")
+print("Pesquisa 2:")
+pesquisa2(0)
+print("Pesquisa 3:")
+pesquisa3(20, 'Mstery')
+print("Pesquisa 4:")
+pesquisa4("feel-god", "prediable")"""
 
 """pesquisa2(0)
 print(1)
